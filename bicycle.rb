3fb11d7e71b3a9249b1d5ea3b21749c7307cd642
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-09-23 00:28:37 +0900

    [3-2]疎結合なコードを書く-インスタンス変数の作成を分離する➀

--- bicycle.rb
+++ bicycle.rb
@@ -1,13 +1,14 @@
 # chainringとcogというデータとraitoを計算するという振る舞いがを持つ「Gear」とうクラスを作る
 class Gear
 
-  # 依存関係にあるコードの書き方
-  attr_reader :chainring, :cog, :wheel
+  attr_reader :chainring, :cog, :rim, :tire
 
-  def initialize(chainring, cog, wheel)
+  # 依存オブジェクトの注入が難しい場合は依存先クラスのインスタンス作成をクラス内で分離する
+  # 今回はWheelクラスのインスタンス作成をinitializeメソッドで行いgear_inchesメソッド内がきれいになる
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
-    @wheel = wheel
+    @wheel = Wheel.new(rim, tire)
   end
 
   # raiio：ギア比（ペダル1漕ぎで車輪が何回転するかを算出する）
@@ -16,8 +17,6 @@ class Gear
   end
 
   def gear_inches
-    # Wheelインスタンスの作成をGearクラスの外部へ移動することでGearとWheelクラス間の結合が切り離される
-    # Gearは変数@wheelに格納された、diameterに応答するオブジェクトであればWheelクラス以外のクラスとも共同作業が可能になった
     ratio * wheel.diameter
   end
 
