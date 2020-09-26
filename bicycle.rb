6f3e4bf3336f4b1e743101241fc1cea6c85fe4e7
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-09-26 14:39:14 +0900

    [3-2]疎結合なコードを書く-引数の順番への依存を取り除く

--- bicycle.rb
+++ bicycle.rb
@@ -1,14 +1,14 @@
 # chainringとcogというデータとraitoを計算するという振る舞いがを持つ「Gear」とうクラスを作る
 class Gear
 
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
 
   # 依存オブジェクトの注入が難しい場合は依存先クラスのインスタンス作成をクラス内で分離する
-  def initialize(chainring, cog, rim, tire)
-    @chainring = chainring
-    @cog = cog
-    @rim = rim
-    @tire = tire
+  # 引数の順番依存を取り除くためにキーワード引数を使う
+  def initialize(**params)
+    @chainring = params[:chaining]
+    @cog = params[:cog]
+    @wheel = params[:wheel]
   end
 
   # raiio：ギア比（ペダル1漕ぎで車輪が何回転するかを算出する）
@@ -51,4 +51,4 @@ class Wheel
 
 end
 
-puts Gear.new(52, 11, Wheel.new(26, 1.5)).gear_inches
+puts Gear.new(chaining: 52, cog: 11, wheel: Wheel.new(26, 1.5)).gear_inches
