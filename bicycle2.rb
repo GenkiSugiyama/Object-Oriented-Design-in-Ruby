bd984c9310fd57914cf56aa7b86dbf47b88f0a47
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-10-20 17:54:48 +0900

    [6-4]抽象を見つける-抽象的な振る舞いを昇格する

--- bicycle2.rb
+++ bicycle2.rb
@@ -1,41 +1,56 @@
-# ロードバイクのみに対応していたBicycleクラスを
-# マウンテンバイクにも対応させる
+# スーパークラスとなるBicycleクラス、BicycleクラスのサブクラスとなるRoadBike、MountainBikeクラスを定義する
+# 中身を書いていたBicycleクラスの名前をRoadBikeに変更し、新しく空のBicycleクラスを作成
 class Bicycle
-  attr_reader :style, :size, :tape_color,
-              :front_shock, :rear_shock
+  attr_reader :size # RoadBikeクラスから昇格
+  
+  def initialize(args={}) # RoadBikeクラスから昇格
+    @size = args[:size]
+  end
+end
+
+class RoadBike < Bicycle
+  attr_reader :tape_color
 
   def initialize(args)
-    @style = args[:style]
-    @size = args[:size]
     @tape_color = args[:tape_color]
-    @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
+    super(args) 
   end
 
   # stylの中身を確認し、if文で振る舞いを分岐させる
   def spares
-    if style == :road
-      {
-        chain: "10-speed",
+      { chain: "10-speed",
         tire_size: "23", # ミリメータ
-        tape_color: tape_color
-      }
-    else
-      {
-        chain: "10-speed",
-        tire_size: "2.1", # インチ
-        rear_shock: rear_shock
-      }
-    end
+        tape_color: tape_color}
   end
+
   # その他のメソッド〜
 end
 
-bike = Bicycle.new(
-  style: :mountain,
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock)
+  end
+end
+
+road_bike = RoadBike.new(
+  size: "M",
+  tape_color: "red"
+)
+
+puts road_bike.spares
+
+mountain_bike = MountainBike.new(
   size: "S",
   front_shock: "Manitou",
   rear_shock: "Fox"
 )
 
-puts bike.spares
+puts mountain_bike.spares
