4a7d152c9f3908ce30de84a23bb15bc4a38b2e23
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-10-21 18:02:20 +0900

    [6 完了]継承によって振る舞いを獲得する

--- bicycle2.rb
+++ bicycle2.rb
@@ -8,6 +8,28 @@ class Bicycle
     @size = args[:size]
     @chain = args[:chain] || default_chain
     @tire_size = args[:tire_size] || default_tire_size
+    # サブクラスの初期化時にsuperを送るのではなく、スーパークラス側でフックメッセージの送信と実装を行う
+    post_initialize(args)
+  end
+
+  def spares
+    # サブクラスの初期化時にsuperを送るのではなく、スーパークラス側でフックメッセージの送信と実装を行う
+    { tire_size: tire_size,
+      chain: chain}.merge(local_spares)
+  end
+
+  def default_tire_size
+    raise NotImplementedError, "This #{self.class} cannnot respond to:"
+  end
+
+  # サブクラスの初期化時にsuperを送るのではなく、スーパークラス側でフックメッセージの送信と実装を行う
+  def post_initialize
+    nil
+  end
+
+  # サブクラスの初期化時にsuperを送るのではなく、スーパークラス側でフックメッセージの送信と実装を行う
+  def local_spares
+    {}
   end
 
   # 全ての自転車はチェーンについて同じ初期値を共有する
@@ -15,35 +37,23 @@ class Bicycle
     '10-speed'
   end
 
-  # テンプレートメソッド（共通するメソッド）は全てのクラスに終えいて実装するよう用意する
-  # 抽象クラスのBicycleクラスでは具体的なタイヤの初期値は必要ないが、エラー文を出力するメソッドとして定義しておく
-  # 分かりやすいエラーメッセージを伴って失敗するコードを書いておくと後から手を加える開発者にとって有益な情報を残せる
-  # 新しいサブクラス作成時、default_tire_sizeを実装していないとエラーになるということを明文化しておく
-  def default_tire_size
-    raise NotImplementedError, "This #{self.class} cannnot respond to:"
-  end
-
-end
-
-class Sample < Bicycle
 end
 
 class RoadBike < Bicycle
   attr_reader :tape_color
 
-  def initialize(args)
+  # スーパークラスで実装されたフックメソッドを具象クラスでオーバーライドする
+  def post_initialize(args)
     @tape_color = args[:tape_color]
-    super(args)
+  end
+
+  # スーパークラスで実装されたフックメソッドを具象クラスでオーバーライドする
+  def local_spares
+    {tape_color: tape_color}
   end
 
   def default_tire_size
     "23"
-  end
-
-  def spares
-      { chain: "10-speed",
-        tire_size: "23", # ミリメータ
-        tape_color: tape_color}
   end
 
   # その他のメソッド〜
@@ -52,34 +62,27 @@ end
 class MountainBike < Bicycle
   attr_reader :front_shock, :rear_shock
 
-  def initialize(args)
+  # スーパークラスで実装されたフックメソッドを具象クラスでオーバーライドする
+  def post_initialize(args)
     @front_shock = args[:front_shock]
     @rear_shock = args[:rear_shock]
-    super(args)
+  end
+
+  # スーパークラスで実装されたフックメソッドを具象クラスでオーバーライドする
+  def local_spares
+    {rear_shock: rear_shock}
   end
 
   def default_tire_size
     "2.1"
   end
 
-  def spares
-    super.merge(rear_shock: rear_shock)
-  end
 end
 
 road_bike = RoadBike.new(
   size: "M",
-  tape_color: "red"
+  tape_color: "red",
+  tire_size: "30"
 )
 
-puts road_bike.spares
-
-mountain_bike = MountainBike.new(
-  size: "S",
-  front_shock: "Manitou",
-  rear_shock: "Fox"
-)
-
-# puts mountain_bike.spares
-
-sample = Sample.new
+puts road_bike.tire_size
